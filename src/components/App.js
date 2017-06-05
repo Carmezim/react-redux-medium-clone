cc20fa1b015514ba107ec405b1c895fc153efb52
Carmezim <carmezim.filho@gmail.com>
2017-06-05 17:29:53 +0100

    Header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,4 @@
+import Header from './Header';
 import React from 'react';
 import { connect } from 'react-redux';
 
@@ -9,7 +10,7 @@ class App extends React.Component {
 	render() {
 		return {
 			<div>
-				{this.props.appName}
+				<Header appName={this.props.appName} />
 			</div>
 		};
 	}
